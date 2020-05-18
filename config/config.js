c3b02c26d3933016fd4b163791bc7b9ae043a848
burhancoulibaly <burhancoulibaly@gmail.com>
2020-05-18 19:43:43 -0400

    fixing config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 try {
     global.client_id = require("./client_id.json");
 } catch (error) {
-    console.log("config.json doesnt exit using environment variables");
+    console.log(error);
     global.client_id = {
         web: {
             client_id: new Buffer.from(process.env.CLIENT_ID, 'base64').toString("ascii").replace(/\\n/g, '\n'),
